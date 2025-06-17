ab32aa1de1998027e92cbc1cda4e2184cc0d5682
goodgreed23 <63224276+goodgreed23@users.noreply.github.com>
2025-06-17 12:38:46 -0700

    Update streamlit_app.py
    change minimum turn into 2, and still show button only after >10 turns

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,7 +35,7 @@ st.set_page_config(page_title="Therapist Chatbot Evaluation", page_icon=None, la
 
 # CONFIGS
 style_id = 0
-min_turns = 20   # number of turns to make before users can save the chat
+min_turns = 2   # number of turns to make before users can save the chat
 MODEL_SELECTED = "gpt-4o"
 
 # Show title and description.
@@ -354,9 +354,10 @@ else:
 
         # if st.button("When the chat feels naturally concluded, click here to save it."):
         #     st.write("**Chat history is saved successfully. You can now use the code ‘COCO123’ to continue the evaluation.**")
-        if st.button("Save Conversation & Start Evaluation"):
-            st.write("**Chat history is saved successfully. You can begin filling out the evaluation questions now.**")
-            st.cache_data.clear()
+              if chat_history_df.shape[0] >= 20:
+                 if st.button("Save Conversation & Start Evaluation"):
+                     st.write("**Chat history is saved successfully. You can begin filling out the evaluation questions now.**")
+                     st.cache_data.clear()
 
         # csv = chat_history_df.to_csv()
         # st.download_button(
