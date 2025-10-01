71570897679dd2f9ee6eae76d390b4a06f551eb9
goodgreed23 <63224276+goodgreed23@users.noreply.github.com>
2025-09-30 17:38:33 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,8 +34,8 @@ import shutil
 st.set_page_config(page_title="Therapist Chatbot Evaluation", page_icon=None, layout="centered", initial_sidebar_state="expanded", menu_items=None)
 
 # CONFIGS
-style_id = 0
-min_turns = 6   # number of turns to make before users can save the chat
+style_id = 6
+min_turns = 2   # number of turns to make before users can save the chat
 MODEL_SELECTED = "gpt-4o"
 
 # Show title and description.
